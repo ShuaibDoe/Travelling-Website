5238db5688d16b41a8afef0d489b66fc25da25c0
ShuaibDoe <ddoedavies@gmail.com>
2023-06-20 17:14:00 +0300

    changes

---
+++ mean.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <?php
+        $x=10;
+        $y=120;
+
+        for($i = 10; $i <=120 - 1; $i += 2) {
+            $sum[] = $evenNumbers[$i] + $evenNumbers[$i + 1];
+        }
+
+        echo "The sum of the even numbers between $x and $y is " . array_
+        
+        
+        ?>
+    </body>
+</html>
